b92c3a29c2e2ba364af0758d33c3de9180698b97
Tanisha Banik <26tanishabanik@gmail.com>
2022-04-16 00:31:09 +0530

    added image

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from main import *
+import base64
 
 st.balloons()
 
@@ -39,16 +40,27 @@ if option == 'Home':
     #       """,
     #       unsafe_allow_html=True
     # )
-    page_bg_img = '''
+    @st.cache(allow_output_mutation=True)
+    def get_base64_of_bin_file(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+    def set_png_as_page_bg(png_file):
+        bin_str = get_base64_of_bin_file(png_file)
+        page_bg_img = '''
         <style>
         body {
-        background-image: url("cats.jpeg");
+        background-image: url("data:image/png;base64,%s");
         background-size: cover;
         }
         </style>
-        '''
+        ''' % bin_str
+        
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+        return
 
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+    set_png_as_page_bg('cats.jpeg')
     st.subheader('OWLS')
     
     st.markdown('Happy Birthday', unsafe_allow_html=True)
