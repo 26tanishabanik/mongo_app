9c34799909a09241476e5460d4c62f335e893ce8
Tanisha Banik <26tanishabanik@gmail.com>
2022-04-15 23:50:49 +0530

    fixed delete function

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ elif option == 'Manage Your Poems':
             #     delete(nameOfThePoem)
             #     st.text('Deleted')
             if nameOfThePoem in getAll():
-                delete(nameOfThePoem)
+                delete({'_id': ({"PoemName": nameOfThePoem})})
                 st.text('Deleted')
 
             else:
